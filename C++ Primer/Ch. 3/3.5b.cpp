c6445998caa2f6668a1e6e09c175db74c7004800
s24306 <s24306@pjwstk.edu.pl>
2022-05-11 19:47:01 +0200

    Add exercise 3.5 from chapter 3

---
+++ C++ Primer/Ch. 3/3.5b.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+
+int main(){
+        std::string str, temp;
+        bool run = true;
+        while(getline(std::cin, temp) && run){
+                if(temp.empty()){
+                        run = false;
+                } else {
+                        str += temp + " ";
+                }
+        }
+        std::cout << str << std::endl;
+        return 0;
+} 
