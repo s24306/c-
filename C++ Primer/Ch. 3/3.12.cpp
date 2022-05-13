78d5a3e865e516bc105fc42f6504196fdfac8303
s24306 <s24306@pjwstk.edu.pl>
2022-05-13 23:09:02 +0200

    Do exercises 3.12-3.13 from ch.3

---
+++ C++ Primer/Ch. 3/3.12.cpp
@@ -0,0 +1,3 @@
+(a) vector<vector<int>> ivec; //creates a vector of vectors that hold int objects
+(b) vector<string> svec = ivec; //error: vectors are of different type (string vs int)
+(c) vector<string> svec(10, "null"); //vector that contains 10 strings of value "null"
---
+++ C++ Primer/Ch. 3/3.13.cpp
@@ -0,0 +1,7 @@
+(a) vector<int> v1; //empty vector
+(b) vector<int> v2(10); //10 elements of val = 0 each
+(c) vector<int> v3(10, 42); //10 elements of val = 42 each
+(d) vector<int> v4{10}; // one element of val = 10
+(e) vector<int> v5{10, 42}; //two elements - one equal to 10, and equal to 42
+(f) vector<string> v6{10}; // one element - string "10"
+(g) vector<string> v7{10, "hi"}; //10 elements - each of value "hi"
